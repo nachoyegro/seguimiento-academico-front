1bc204766778f811c3ab2e569b6bb21b0e563b33
Juan Ignacio Yegro <juan.yegro@unq.edu.ar>
2019-05-16 14:08:12 -0300

    Componente basico

---
+++ src/components/Alumno.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Alumno extends Component {
+    render(){
+        return <p>Esto es un alumno</p>;
+    }
+}
+
+export default Alumno;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Alumno from './components/Alumno.js'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+render(<Alumno/>, document.querySelector('#root'));
