ef3768191b92d0e3b18b763336f461b3f590e410
Juan Ignacio Yegro <juan.yegro@unq.edu.ar>
2019-08-23 17:36:41 -0300

    Listado basico de alumnos

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {alumnos: []}
+    }
+
+    componentDidMount() {
+        axios.get(`http://127.0.0.1:8000/api/v2/alumnos/?format=json`)
+          .then(res => {
+            const alumnos = res.data;
+            this.setState({ alumnos: alumnos });
+          })
+    }
+
+    render() {
+        return (
+            <ul>
+                { this.state.alumnos.map(alumno => <li>{alumno.nombre}</li>)}
+            </ul>
+        );
+    }
 }
 
 export default App;
+
