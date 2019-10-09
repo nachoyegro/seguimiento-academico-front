5f0d5359cf3a3ad94c27f50ba3e886eeb43433dd
Juan Ignacio Yegro <nachoyegro@gmail.com>
2019-10-08 21:21:32 -0300

    ENV

--- src/index.js
+++ src/index.js
@@ -3,4 +3,4 @@ import { render } from 'react-dom';
 import Alumno from './components/Alumno.js';
 import GraficoDeLineas from './components/GraficoDeLineas.js';
 
-render(<GraficoDeLineas titulo={'Indice de aprobación'} url={'http://127.0.0.1:5000/indiceaprobacion'}/>, document.querySelector('#root'));
+render(<GraficoDeLineas titulo={'Indice de aprobación'} url={process.env.REACT_APP_INDICE_APROBACION_URL}/>, document.querySelector('#root'));
