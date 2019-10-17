03729caf74daa489179ab05a42ae35c32b198fc7
Juan Ignacio Yegro <nachoyegro@gmail.com>
2019-10-17 16:14:47 -0300

    Login basico, recibiendo token

---
+++ src/components/AuthenticatedComponent.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { AxiosProvider } from 'react-axios';
+import axios from 'axios';
+import { getJWT } from '../helpers/jwt';
+ 
+const axiosInstance = axios.create({
+    headers: { 'Authorization': `Bearer ${getJWT()}` }
+  });
+
+class AuthenticatedComponent extends Component {
+
+  render(){
+    return (<AxiosProvider instance={axiosInstance}>
+                <div>{this.renderComponent()}</div>
+            </AxiosProvider>
+        )
+  }
+}
+
+export default AuthenticatedComponent;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,10 +1,51 @@
-import React, {Component} from 'react';
+import React, {Component} from "react";
+import axios from 'axios';
+import qs from 'qs';
 
 class Login extends Component {
-    render() {
-        return <h1>Login Page</h1>
 
-    }
+  state = {
+    username: "",
+    password: "",
+  }
+
+  submit = e => {
+      e.preventDefault();
+      const url = process.env.REACT_APP_TOKEN_URL;
+      const data = { 'username': this.state.username, 'password': this.state.password };
+      const options = {
+        method: 'POST',
+        headers: { 'content-type': 'application/x-www-form-urlencoded' },
+        data: qs.stringify(data),
+        url,
+      };
+      axios(options).then( res => localStorage.setItem('jwt', res.data.access)).catch( error => console.log(error.response));
+  }
+
+  render() {
+    return (
+      <form onSubmit={ e => this.submit(e)}>
+        <fieldset>
+          <legend>Login</legend>
+          <p>
+            <label htmlFor="username">Username</label>
+            <input
+              type="text" id="username"
+              onChange={e => this.setState({username: e.target.value})} />
+          </p>
+          <p>
+            <label htmlFor="password">Password</label>
+            <input
+              type="password" id="password"
+              onChange={e => this.setState({password: e.target.value})} />
+          </p>
+          <p>
+            <button type="submit">Login</button>
+          </p>
+        </fieldset>
+      </form>
+    )
+  }
 }
 
 export default Login;
