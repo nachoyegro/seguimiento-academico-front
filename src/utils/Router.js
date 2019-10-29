d551c28a10a9a9c4b83044fb67a19b50c5521070
Juan Ignacio Yegro <nachoyegro@gmail.com>
2019-10-29 20:45:47 -0300

    Cambios en auth con redirect sin jwt

--- src/utils/Router.js
+++ src/utils/Router.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import EstadisticasMateriaPage from '../pages/EstadisticasMateriaPage';
 import Login from '../pages/Login';
 import Logout from '../pages/Logout';
+import HomePage from '../pages/HomePage';
 import { ProtectedRoute } from './ProtectedRoute';
 
 const Router = () => (
   <BrowserRouter>
     <Switch>
       <Route exact path="/login" component={Login} />
+      <ProtectedRoute exact path="/" component={HomePage} />
       <ProtectedRoute exact path="/estadisticas" component={EstadisticasMateriaPage} />
       <ProtectedRoute exact path="/logout" component={Logout} />
     </Switch>
