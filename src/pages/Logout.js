d0b6f994d39bd270521c9e76f7d7b452a355c693
Juan Ignacio Yegro <nachoyegro@gmail.com>
2020-06-05 10:01:58 -0300

    Saco token en logout

--- src/pages/Logout.js
+++ src/pages/Logout.js
@@ -1,12 +1,12 @@
 import React from "react";
 import BasePage from '../pages/BasePage';
 import Button from '@material-ui/core/Button';
-
+import { removeJWT } from '../helpers/auth';
 
 class Logout extends BasePage {
 
   logout = e => {
-    localStorage.removeItem('jwt');
+    removeJWT();
     this.props.history.push('/login');
   }
 
@@ -16,13 +16,15 @@ class Logout extends BasePage {
 
   renderPage() {
     return (
-      <fieldset className="espaciado-arriba">
-        <legend>¿Está seguro que desea salir?</legend>
-        <p className="center">
-          <Button className="separado" variant="contained" onClick={e => this.logout(e)}>Si</Button>
-          <Button className="separado" variant="contained" onClick={e => this.cancel(e)}>No</Button>
-        </p>
-      </fieldset>
+      <div className="form-organigrama center">
+        <fieldset className="espaciado-arriba">
+          <legend>¿Está seguro que desea salir?</legend>
+          <p className="center botonera-logout">
+            <Button className="separado" variant="contained" onClick={e => this.logout(e)}>Si</Button>
+            <Button className="separado" variant="contained" onClick={e => this.cancel(e)}>No</Button>
+          </p>
+        </fieldset>
+      </div>
     )
   }
 }
