85eda71ef860f8accda8149b4fd755fd2c42489d
Juan Ignacio Yegro <nachoyegro@gmail.com>
2020-06-05 09:34:55 -0300

    Dominio de grafico de dispersion

--- src/components/GraficoDeDispersion.js
+++ src/components/GraficoDeDispersion.js
@@ -7,20 +7,20 @@ import {
 
 class GraficoDeDispersion extends CustomChart {
 
-    renderChart(data) {
+  renderChart(data) {
     return (<ScatterChart
-                width="100%" 
-                height="100%"
-                margin={{
-                    top: 10, right: 30, left: 0, bottom: 0,
-                }}
-            >
-                <CartesianGrid />
-                <XAxis type="number" dataKey={this.props.dataX} name={this.props.nombreX} unit={this.props.unitX} />
-                <YAxis type="number" dataKey={this.props.dataY} name={this.props.nombreY} unit={this.props.unitY} />
-                <Tooltip cursor={{ strokeDasharray: '1 1' }} />
-                <Scatter name={this.props.titulo} data={data} fill="#29ABE2" />
-            </ScatterChart>
+      width="100%"
+      height="100%"
+      margin={{
+        top: 10, right: 30, left: 0, bottom: 0,
+      }}
+    >
+      <CartesianGrid />
+      <XAxis type="number" dataKey={this.props.dataX} name={this.props.nombreX} unit={this.props.unitX} domain={[0, 10]} />
+      <YAxis type="number" dataKey={this.props.dataY} name={this.props.nombreY} unit={this.props.unitY} domain={[0, 10]} />
+      <Tooltip cursor={{ strokeDasharray: '1 1' }} />
+      <Scatter name={this.props.titulo} data={data} fill="#29ABE2" />
+    </ScatterChart>
     );
   }
 }
