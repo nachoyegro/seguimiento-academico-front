7bc98e85b22213f7bcf0b333dba89325774f3553
Juan Ignacio Yegro <nachoyegro@gmail.com>
2020-06-07 11:07:52 -0300

    Cambio email por nombre de usuario

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -12,7 +12,7 @@ import axios from 'axios';
 import qs from 'qs';
 import { useHistory } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
-import logo from '../logo.png'; 
+import logo from '../logo.png';
 import theme from '../styles/Theme';
 
 const useStyles = makeStyles((theme) => ({
@@ -40,16 +40,16 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 export default function SignIn(initialState) {
-    const [email, setEmail] = useState(initialState);
-    const [password, setPassword] = useState(initialState);
-    const classes = useStyles();
-    const history = useHistory();
+  const [username, setUsername] = useState(initialState);
+  const [password, setPassword] = useState(initialState);
+  const classes = useStyles();
+  const history = useHistory();
 
 
   const submit = e => {
     e.preventDefault();
     const url = process.env.REACT_APP_TOKEN_URL;
-    const data = { 'username': email, 'password': password };
+    const data = { 'username': username, 'password': password };
     const options = {
       method: 'POST',
       headers: { 'content-type': 'application/x-www-form-urlencoded' },
@@ -71,7 +71,7 @@ export default function SignIn(initialState) {
         <div className={classes.paper}>
           <img className={classes.logo} src={logo}></img>
           <Typography component="h1" variant="h5">
-          Ingresar
+            Ingresar
           </Typography>
           <form className={classes.form} onSubmit={e => submit(e)}>
             <TextField
@@ -79,11 +79,11 @@ export default function SignIn(initialState) {
               margin="normal"
               required
               fullWidth
-              id="email"
-              label="Email"
-              name="email"
-              autoComplete="email"
-              onChange={e => setEmail(e.target.value)} 
+              id="username"
+              label="Nombre de Usuario"
+              name="Nombre de Usuario"
+              autoComplete="username"
+              onChange={e => setUsername(e.target.value)}
               autoFocus
             />
             <TextField
